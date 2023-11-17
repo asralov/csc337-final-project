9da47a79b5bcf0bc655e333bd435d9a606048b01
pulyau <83565561+pulyau@users.noreply.github.com>
2023-11-17 00:33:11 -0700

    implemented login and sign up methods
    added login and signup methods

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,59 +7,94 @@ const { PassThrough } = require('stream');
 
 // I was thinking adminAuth will be required to pull user data then userAuth will be required to update user data
 
-// GET request - get all users
-router.get('/', authenticate.adminAuth(), async (req, res) => {
-    try {
-        const users = await User.find();
-        res.json(users);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
+// // GET request - get all users
+// router.get('/', authenticate.adminAuth(), async (req, res) => {
+//     try {
+//         const users = await User.find();
+//         res.json(users);
+//     } catch (err) {
+//         res.status(500).json({ message: err.message });
+//     }
+// });
 
-// GET request - get a single user by ID
-router.get('/:id', authenticate.adminAuth(), async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id);
-        if (user) {
-            res.json(user);
-        } else {
-            res.status(404).json({ message: 'User not found' });
-        }
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
+// // GET request - get a single user by ID
+// router.get('/:id', authenticate.adminAuth(), async (req, res) => {
+//     try {
+//         const user = await User.findById(req.params.id);
+//         if (user) {
+//             res.json(user);
+//         } else {
+//             res.status(404).json({ message: 'User not found' });
+//         }
+//     } catch (err) {
+//         res.status(500).json({ message: err.message });
+//     }
+// });
 
-// POST request - add a new user
-router.post('/add', authenticate.adminAuth(), async (req, res) => {
-    //Still working on this function kinda need to figure out how to add a profile picture
-    // And how the req body will look like
-});
+// // POST request - add a new user
+router.post('/add', (req, res) => { 
+    console.log("uhuh");
+    let username = req.body.username;
+    console.log(req.body);
+    let password = req.body.password;
+    let p = User.find({username: username}).exec()
+    p.then((result) => {
+      // Checks if the user already exists
+      if (result.length != 0) {
+        res.end(("USER ALREADY EXISTS"));
+      } else {
+        let user = new User({username: username});
+        user.save();
+        res.end('USER SUCCESSFULLY SAVED');
+      }
+    }).catch((err) => {
+      res.end("USER SAVE ERROR");
+    })
+  });
+
+// POST method. Logs the user into the website.
+router.post('/login', (req, res) => { 
+    console.log("yessir");
+    let u = req.body;
+    // Checks if the user is in the database
+    let p1 = User.find({username: u.username, password: u.password}).exec();
+    p1.then( (results) => { 
+      if (results.length == 0) {
+        res.end('Coult not find account');
+      } else {
+        let sid = addSession(u.username);  
+        // Creates a cookie with 
+        res.cookie("login", 
+          {username: u.username, sessionID: sid}, 
+          {maxAge: 60000 * 5 });
+        res.end('SUCCESS');
+      }
+    });
+  });
 
 
-// PUT request - update a user
-router.put('/update/:id', authenticate.userAuth(), async (req, res) => {
-    try {
-        const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        res.json(updatedUser);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-});
+// // PUT request - update a user
+// router.put('/update/:id', authenticate.userAuth(), async (req, res) => {
+//     try {
+//         const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+//         res.json(updatedUser);
+//     } catch (err) {
+//         res.status(400).json({ message: err.message });
+//     }
+// });
 
-// DELETE request - delete a user
-router.delete('/delete/:id', authenticate.userAuth(), async (req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id);
-        if (user) {
-            res.json({ message: 'User deleted successfully' });
-        } else {
-            res.status(404).json({ message: 'User not found' });
-        }
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
+// // DELETE request - delete a user
+// router.delete('/delete/:id', authenticate.userAuth(), async (req, res) => {
+//     try {
+//         const user = await User.findByIdAndDelete(req.params.id);
+//         if (user) {
+//             res.json({ message: 'User deleted successfully' });
+//         } else {
+//             res.status(404).json({ message: 'User not found' });
+//         }
+//     } catch (err) {
+//         res.status(500).json({ message: err.message });
+//     }
+// });
 
 module.exports = router;
