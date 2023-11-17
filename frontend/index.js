c8682eb9dafaecafd043f10ec3ab87c101102281
Abrorjon Asralov <113268353+asralov@users.noreply.github.com>
2023-11-17 09:40:37 -0700

    adding the main login page
    adding a new login page that is ready and need to add a retrieving data function from the user in a case he is not registered or give a permission and start a cookie session

---
+++ frontend/index.js
@@ -0,0 +1,29 @@
+const createAcc = document.getElementById("reg-link");
+let signUp = false;
+// getting main components
+const st = document.getElementById("statusTitle");
+const btn = document.getElementById("btn");
+const promt = document.getElementById("logreg-link");
+
+function getCreatePage(){
+    st.textContent = "Create Account";
+    btn.innerHTML = `<button type="submit" id="btn">
+                        Sign Up
+                    </button>`;
+    promt.innerHTML =  `<p>
+                            Already got an account? <button onclick="getLoginPage()" id="reg-link">Login</button>
+                        </p>`;
+    document.getElementById("user").value = "";
+    document.getElementById("password").value = "";
+}
+function getLoginPage(){
+    st.textContent = "Login";
+    btn.innerHTML = `<button type="submit" id="btn">
+                        Login
+                    </button>`;
+    promt.innerHTML =  `<p>
+                            Don't have an account? <button onclick="getCreatePage()" id="reg-link">Sign Up</button>
+                        </p>`;
+    document.getElementById("user").value = "";
+    document.getElementById("password").value = "";
+}
