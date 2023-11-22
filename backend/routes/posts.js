88ca738867a58a011399d562110545a8effa3ccd
Jabaar <michael.sc.evans@gmail.com>
2023-11-21 23:15:28 -0700

    Change return value for post fetching

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -6,23 +6,25 @@ const Post = require('../models/Post');
 router.post('/add', async (req, res) => {
     try {
         const post = new Post(req.body);
+        
         await post.save();
-        res.status(201).json({ message: 'Post created successfully', post });
+        res.status(201).json({ message: 'Post created successfully', _id: post._id });
     } catch (error) {
         res.status(500).json({ message: 'Error adding post', error: error.message });
     }
 });
 
-
 // Edit an existing post
 router.post('/edit/:id', async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
-        if (!post) {
+
+        if (!post)
             return res.status(404).json({ message: 'Post not found' });
-        }
+
         post.title = req.body.title;
         post.content = req.body.content;
+
         await post.save();
         res.json({ message: 'Post updated successfully', post });
     } catch (error) {
@@ -34,9 +36,10 @@ router.post('/edit/:id', async (req, res) => {
 router.delete('/delete/:id', async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
-        if (!post) {
+        
+        if (!post)
             return res.status(404).json({ message: 'Post not found' });
-        }
+
         await post.remove();
         res.json({ message: 'Post deleted successfully' });
     } catch (error) {
@@ -48,6 +51,7 @@ router.delete('/delete/:id', async (req, res) => {
 router.get('/all', async (req, res) => {
     try {
         const posts = await Post.find({});
+
         res.json(posts);
     } catch (error) {
         res.status(500).json({ message: 'Error fetching posts', error });
@@ -58,9 +62,10 @@ router.get('/all', async (req, res) => {
 router.get('/:id', async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
-        if (!post) {
+
+        if (!post)
             return res.status(404).json({ message: 'Post not found' });
-        }
+
         res.json(post);
     } catch (error) {
         res.status(500).json({ message: 'Error fetching post', error });
