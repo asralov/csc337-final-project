8254f350a4f5eb6035d87332b561f5c4a2a639ec
Ryder Rhoads <ryder.rhoads10@gmail.com>
2023-11-27 13:49:45 -0700

    added test

--- backend/py/SearchTopic.py
+++ backend/py/SearchTopic.py
@@ -18,10 +18,11 @@ SID = SentimentIntensityAnalyzer()
 
 
 class SearchTopic:
-    def __init__(self, topic):
+    def __init__(self, topic,similarity_threshold):
         # save topic term and get articles
         self._term = topic
         self._articles = self.get_articles()
+        self._similarity_threshold=similarity_threshold
 
     def get_articles(self, num_loop=1):
         """Function to download articles sorted by time from the newsAPI on a given topic"""
@@ -83,7 +84,7 @@ class SearchTopic:
         cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
         return cosine_sim
 
-    def find_article_groups(self, threshold=0.5):
+    def find_article_groups(self):
         """Function to find groups of similar articles"""
         similarity_scores = self.calculate_similarity()
         num_articles = len(similarity_scores)
@@ -102,7 +103,7 @@ class SearchTopic:
             # Compare the current article with every other article
             for j in range(num_articles):
                 # If the similarity score exceeds the threshold and is not the same article, add it to the similar articles list
-                if i != j and similarity_scores[i][j] > threshold:
+                if i != j and similarity_scores[i][j] > self._similarity_threshold:
                     similar_articles.append(j)
                     grouped_articles.add(j)
 
@@ -114,7 +115,17 @@ class SearchTopic:
 
     def create_prompt(self, articles):
         """Function to create prompt for GPT-3.5"""
-        prompt = f'Below are summaries of articles related to {self._term}. Please provide a short summary for all of them and return back a JSON which has a general title listed as "title", then "background" which is a background of the topic, followed by the summary of the articles listed as "summary". Again, I only need 1 summary, background, and title for all of the articles and just the JSON which will be title: String, background:String, summary: String, as I will be using the response in a program. It should be short and like a debrief for the president. If there are any articles which don\'t seem to align with the general idea / topic please ignore them. \n\n'
+        prompt = (
+            f'Summarize articles related to {self._term} in a concise format suitable for a high-level briefing. '
+            'Return the data in a JSON structure with the following fields: '
+            '"title" for a general title, "background" for an overview of the topic, '
+            '"summary" for a combined summary of all relevant articles, and "topics" to list applicable categories. '
+            'The categories include: economics, technology, politics, health, business, sports, entertainment, science, world. '
+            'The summary should encapsulate the key points from all pertinent articles. '
+            'Exclude any articles that do not align with the central theme of the topic. '
+            'The final output should be concise, providing a succinct debriefing. '
+            'The JSON structure should be as follows: {title: String, background: String, summary: String, topics: Array}. '
+            'This format is intended for integration into a program. So please provide just the JSON structure.')
         for article in articles:
             json_article = self._articles[article]
             prompt += f"\n\nTitle: {json_article['title']}\n"
@@ -124,22 +135,27 @@ class SearchTopic:
 
     def export_GPT_summaries(self):
         """Function to export GPT-3 summaries to text files, along with the summaries from the articles"""
-        article_groups = self.find_article_groups(threshold=0.6)
+        jsons=[]
+        article_groups = self.find_article_groups()
         for group_id, article_indices in article_groups.items():
-            prompt = self.create_prompt(article_indices)
-            summary = self.generate_summary(prompt)
-            file_name = f"Articles/group_{group_id}_{self._term}.json"
-            urls = [self._articles[article_index]['url'] for article_index in article_indices]
-            summary_json = {
-                "GPT_response": json.loads(summary),
-                "term": self._term,
-                "sentiment": self._articles[article_indices[0]]["sentiment"],
-                "urls": urls
-            }
-
-            with open(file_name, "w", encoding="utf-8") as file:
-                # Write the JSON object to the file
-                json.dump(summary_json, file, indent=4, ensure_ascii=False)
+            try:
+                prompt = self.create_prompt(article_indices)
+                summary = self.generate_summary(prompt)
+                file_name = f"Articles/group_{group_id}_{self._term}.json"
+                urls = [self._articles[article_index]['url'] for article_index in article_indices]
+                print(summary)
+                summary_json = {
+                    "GPT_response": json.loads(summary),
+                    "sentiment": self._articles[article_indices[0]]["sentiment"],
+                    "urls": urls
+                }
+                with open(file_name, "w", encoding="utf-8") as file:
+                    # Write the JSON object to the file
+                    json.dump(summary_json, file, indent=4, ensure_ascii=False)
+                jsons.append(summary_json)
+            except Exception as e:
+                print(f"Error exporting topic group: {e}")
+        return jsons
 
 
     def generate_summary(self, prompt):
--- backend/py/testUsage.py
+++ backend/py/testUsage.py
@@ -1,15 +1,26 @@
 from SearchTopic import SearchTopic
-from TrendingTopic import TrendingTopic
+import requests
 
 # Testing Usage Search Topic
-econ = SearchTopic(topic="Economics")
-musk = SearchTopic(topic="Elon Musk")
-econ.export_GPT_summaries()
-musk.export_GPT_summaries()
+def test_stories():
+    stories = SearchTopic(topic="israel hamas conflict",similarity_threshold=0.7)
+    summarizes=stories.export_GPT_summaries()
+    print(summarizes)
+    #send_stories_to_db(econ_summarizes)
 
-# Testing Usage Top Topics
-hamas = TrendingTopic(country="us",catagory="general",topic="Hamas")
-AAPL = TrendingTopic(country="us",catagory="general",topic="AAPL")
+def send_stories_to_db(stories):
+    for story in stories:
+        title=story["GPT_response"]["title"]
+        background=story["GPT_response"]["background"]
+        summary=story["GPT_response"]["summary"]
+        urls=list(story["urls"])
+        topics=list(story["GPT_response"]["topics"])
+        
+        #Orgainize json
+        json={"title":title, 
+            "content":{background:background,summary:summary},
+            "urls":urls,
+            "topics":topics}
+        requests.post("/post/add", json=json)
 
-hamas.export_GPT_summaries()
-AAPL.export_GPT_summaries()
+test_stories()
