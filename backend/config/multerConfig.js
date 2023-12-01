439d0b7058437cdcdfbf3dcb16eca972d12ddd22
Jabaar <michael.sc.evans@gmail.com>
2023-11-30 19:24:09 -0700

    Change file name scheme

--- backend/config/multerConfig.js
+++ backend/config/multerConfig.js
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
     },
     filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+        cb(null, req.cookies.login.username + '-' + uniqueSuffix + path.extname(file.originalname));
     }
 });
 
