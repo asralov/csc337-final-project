85d32bdde0216df13f0f9878a155aaf4f1977cd7
Ryder Rhoads <ryder.rhoads10@gmail.com>
2023-12-06 11:32:18 -0700

    https

--- backend/server.js
+++ backend/server.js
@@ -23,14 +23,14 @@ const app = express();
 const fs = require('fs');
 
 // HTTPS Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/losethebias.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/losethebias.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/losethebias.com/chain.pem', 'utf8');
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
-};
+//const privateKey = fs.readFileSync('/etc/letsencrypt/live/losethebias.com/privkey.pem', 'utf8');
+//const certificate = fs.readFileSync('/etc/letsencrypt/live/losethebias.com/cert.pem', 'utf8');
+//const ca = fs.readFileSync('/etc/letsencrypt/live/losethebias.com/chain.pem', 'utf8');
+//const credentials = {
+//	key: privateKey,
+//	cert: certificate,
+//	ca: ca
+//};
 
 // MongoDB connection setup
 mongoose.connect('mongodb://127.0.0.1:27017/losethebias', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -57,12 +57,12 @@ app.use(express.static('../frontend/'));
 
 // Starting both http & https servers
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
+//const httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(80, () => {
 	console.log('HTTP Server running on port 80');
 });
 
-httpsServer.listen(443, () => {
-	console.log('HTTPS Server running on port 443');
-});
+//httpsServer.listen(443, () => {
+//	console.log('HTTPS Server running on port 443');
+//});
